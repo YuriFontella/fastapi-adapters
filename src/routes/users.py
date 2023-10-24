5d59adf3ad456bf9cf84a0995672027401b056a6
Yuri Fontella <yurifc4@gmail.com>
2023-10-24 11:44:33 -0300

    database

--- src/routes/users.py
+++ src/routes/users.py
@@ -1,11 +1,10 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter
+from config.pool import database
 
 router = APIRouter()
 
 @router.get('/users')
-async def users(request: Request):
-    pool = request.app.state.pool
-    async with pool.acquire() as conn:
-        rows = await conn.fetch('SELECT name FROM users LIMIT 1')
+async def users():
+    rows = await database.fetch_one('SELECT name FROM users LIMIT 1')
 
     return rows
