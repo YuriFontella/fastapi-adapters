c5b93b29f1fbd1863ee6770000c514343a7ce341
Yuri Fontella <yurifc4@gmail.com>
2023-11-14 09:36:35 -0300

    pool wait

--- config/pool.py
+++ config/pool.py
@@ -1,7 +1,6 @@
 import configparser, os
 
 from psycopg_pool import AsyncConnectionPool
-from psycopg.rows import dict_row
 from contextlib import asynccontextmanager
 
 config = configparser.ConfigParser()
@@ -9,11 +8,11 @@ config.read('config.ini')
 
 env = os.environ.get('PYTHON_ENV', 'development')
 
-pool = AsyncConnectionPool(conninfo=config.get(env, 'CONNINFO'), open=False, min_size=2, max_size=4)
+pool = AsyncConnectionPool(conninfo=config.get(env, 'CONNINFO'), min_size=2, max_size=4)
 
 @asynccontextmanager
 async def lifespan(app):
-    await pool.open()
+    await pool.wait()
     yield
 
     await pool.close()
