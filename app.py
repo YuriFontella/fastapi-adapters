c0e553ff9a5679e82b9e41ac68c74eb046e26c06
Yuri Fontella <yurifc4@gmail.com>
2023-12-30 11:14:23 -0300

    exception_handler

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ from fastapi.responses import JSONResponse
 from config.pool import lifespan
 from src.routes import users
 
-app = FastAPI(lifespan=lifespan)
-
-app.include_router(users.router)
-
-@app.exception_handler(Exception)
 async def exception_handler(request, exc):
     return JSONResponse(
         status_code=500,
         content={'message': 'Internal Server Error'},
     )
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(users.router)
+app.add_exception_handler(Exception, exception_handler)
